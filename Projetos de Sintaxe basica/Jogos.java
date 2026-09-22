87cb6cadc1fa3ebe1443d61d55a11695b923a948
TllFabri <thiagobarrosfabri07@gmail.com>
2026-09-22 00:22:47 -0300

    jogos.java

---
+++ Projetos de Sintaxe basica/Jogos.java
@@ -0,0 +1,8 @@
+public class Jogos {
+    public static void main (String[] args) {
+        System.out.println("Lista de Jogos");
+
+        String jogo1 = "zelda tears of the kingdom"
+    }
+}
+|

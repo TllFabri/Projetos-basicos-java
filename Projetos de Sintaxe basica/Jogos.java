8282845161920fb97ba7ee700f484d2ef988d2e5
TllFabri <thiagobarrosfabri07@gmail.com>
2026-09-22 00:27:20 -0300

    zelda tears of the kingdom

--- Projetos de Sintaxe basica/Jogos.java
+++ Projetos de Sintaxe basica/Jogos.java
@@ -2,7 +2,14 @@ public class Jogos {
     public static void main (String[] args) {
         System.out.println("Lista de Jogos");
 
-        String jogo1 = "zelda tears of the kingdom"
+        String jogo1 = "zelda tears of the kingdom";
+        System.out.println(jogo1);
+
+        String nota1 = "nota: 10/10";
+        System.out.println(nota1);
+
+        String tempo1 = "tempo de jogo: 100 horas";
+        System.out.println(tempo1);
+
     }
 }
-|
